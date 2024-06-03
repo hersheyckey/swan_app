fc2fb858b47aa44ae07d0c388121bb5d59ae9268
harshad <hersheyckey@gmail.com>
2024-06-03 16:36:05 +0530

    svg_to png

--- lib/resources/asset_manager.dart
+++ lib/resources/asset_manager.dart
@@ -3,7 +3,7 @@ const String IMAGE_PATH = "assets/images";
 
 class AssetManager {
   static const String loginBG = "$IMAGE_PATH/bg_login.png";
-  static const String logo = "$IMAGE_PATH/Logo.svg";
+  static const String logo = "$IMAGE_PATH/Logo.png";
   static const String searchICon = "$SVG_PATH/search.svg";
   static const String favouriteIcon = "$SVG_PATH/favourite.svg";
   static const String cartIcon = "$SVG_PATH/cart.svg";
